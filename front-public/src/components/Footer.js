88e15b34080c17901603f95872f2468f847fd65a
Eva Spessotto <eva@spessotto.com>
2019-01-23 11:58:35 +0100

    Changement cards et footer

--- front-public/src/components/Footer.js
+++ front-public/src/components/Footer.js
@@ -4,32 +4,54 @@ import { Segment, Container, Grid, Header, Icon, Item, Divider } from 'semantic-
 class Footer extends Component {
   render() {
     return (
-      <Container textAlign='center'>
-        <Grid divided columns='equal' stackable relaxed='very' verticalAlign='middle'>
-          <Grid.Column>
-            <Header>
-              <Icon name='map marker alternate'/>
-              28 grande rue Saint-Nicolas, Toulouse
-            </Header>
-            <Header>
-              <Icon name='phone'/>
-              085625729
-            </Header>
-            <Header>
-              <Icon name='mail'/>
-                shopping@contact.com
-            </Header>
-          </Grid.Column>
-          <Grid.Column textAlign='left' >
-            <Header >About the company</Header>
-            <p>Aliqua amet qui eiusmod enim exercitation sunt laboris. Deserunt id officia eiusmod magna esse aliquip. Cupidatat dolor reprehenderit fugiat et Lorem commodo. Qui ut aliquip fugiat tempor irure enim ex.</p>
-            <p>Aliqua amet qui eiusmod enim exercitation sunt laboris. Deserunt id officia eiusmod magna esse aliquip. Cupidatat dolor reprehenderit fugiat et Lorem commodo. Qui ut aliquip fugiat tempor irure enim ex.</p>
-            <Icon name='facebook square' size='big'/>
-            <Icon name='instagram' size='big'/>
-            <Icon name='twitter square' size='big'/>
-          </Grid.Column>
-        </Grid>     
-      </Container> 
+      <Container style={{marginBottom: '20px'}}>
+        <Divider />
+        <Grid style={{marginTop: '20px'}} stackable>
+          <Grid.Row columns='4'>
+            <Grid.Column>
+              <Header>About</Header>
+              <p>About us</p>
+              <p>Carrer</p>
+              <p>Corporate Sales</p>
+              <p>Where to buy</p>
+            </Grid.Column>
+            <Grid.Column>
+              <Header>Support</Header>
+              <p>Contact</p>
+              <p>Returns</p>
+              <p>Warranty</p>
+              <p>Help</p>
+            </Grid.Column>
+            <Grid.Column>
+              <Header>Legal</Header>
+              <p>Privacy</p>
+              <p>Terms</p>
+              <p>Patends</p>
+            </Grid.Column>
+            <Grid.Column>
+              <Header>Other</Header>
+              <p>Account</p>
+              <p>Affiliate Program</p>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row textAlign='center' style={{marginTop: '50px'}}>
+            <Grid.Column>
+              <Icon name='facebook' size='large' />
+              <Icon name='instagram' size='large' />
+              <Icon name='youtube' size='large' />
+              <Icon name='twitter' size='large' />
+              <Icon name='pinterest' size='large' />
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row textAlign='center'>
+            <Grid.Column>
+            <p>@2019 - All right reserved</p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
     );
   }
 }
--- front-public/src/components/Home/HomeProductsList.js
+++ front-public/src/components/Home/HomeProductsList.js
@@ -1,133 +1,105 @@
 import React, { Fragment } from 'react';
-import { Button, Card, Image, Placeholder, Container } from 'semantic-ui-react'
+import { Button, Card, Image, Placeholder, Container, Icon, Header } from 'semantic-ui-react'
 
 const HomeProductsList = () => {
   return (
-    <Container style={{marginTop: '100px', marginBottom: '200px'}}>
+    <Container style={{ marginTop: '100px', marginBottom: '200px' }}>
       <Card.Group centered itemsPerRow='4' stackable>
         <Card>
           <Placeholder>
             <Placeholder.Image square />
           </Placeholder>
-
           <Card.Content>
-            <Placeholder>
-              <Placeholder.Header>
-                <Placeholder.Line length='very short' />
-                <Placeholder.Line length='medium' />
-              </Placeholder.Header>
-              <Placeholder.Paragraph>
-                <Placeholder.Line length='short' />
-              </Placeholder.Paragraph>
-            </Placeholder>
+            <Card.Header>Truc à vendre</Card.Header>
+            <Card.Meta>
+              <span className='price'>60€</span>
+            </Card.Meta>
+            <Card.Description>Un truc dont vous avez vraiment besoin</Card.Description>
           </Card.Content>
-
-          <Card.Content extra>
-            <Button primary>
-              Add
+          <Button animated='vertical' primary>
+            <Button.Content hidden>Ajouter au panier</Button.Content>
+            <Button.Content visible>
+              <Icon name='shop' />
+            </Button.Content>
           </Button>
-            <Button>Delete</Button>
-          </Card.Content>
         </Card>
 
         <Card>
           <Placeholder>
             <Placeholder.Image square />
           </Placeholder>
-
           <Card.Content>
-            <Placeholder>
-              <Placeholder.Header>
-                <Placeholder.Line length='very short' />
-                <Placeholder.Line length='medium' />
-              </Placeholder.Header>
-              <Placeholder.Paragraph>
-                <Placeholder.Line length='short' />
-              </Placeholder.Paragraph>
-            </Placeholder>
+            <Card.Header>Truc à vendre</Card.Header>
+            <Card.Meta>
+              <span className='price'>60€</span>
+            </Card.Meta>
+            <Card.Description>Un truc dont vous avez vraiment besoin</Card.Description>
           </Card.Content>
-
-          <Card.Content extra>
-            <Button primary>
-              Add
+          <Button animated='vertical' primary>
+            <Button.Content hidden>Ajouter au panier</Button.Content>
+            <Button.Content visible>
+              <Icon name='shop' />
+            </Button.Content>
           </Button>
-            <Button>Delete</Button>
-          </Card.Content>
         </Card>
 
         <Card>
           <Placeholder>
             <Placeholder.Image square />
           </Placeholder>
-
           <Card.Content>
-            <Placeholder>
-              <Placeholder.Header>
-                <Placeholder.Line length='very short' />
-                <Placeholder.Line length='medium' />
-              </Placeholder.Header>
-              <Placeholder.Paragraph>
-                <Placeholder.Line length='short' />
-              </Placeholder.Paragraph>
-            </Placeholder>
+            <Card.Header>Truc à vendre</Card.Header>
+            <Card.Meta>
+              <span className='price'>60€</span>
+            </Card.Meta>
+            <Card.Description>Un truc dont vous avez vraiment besoin</Card.Description>
           </Card.Content>
-
-          <Card.Content extra>
-            <Button primary>
-              Add
+          <Button animated='vertical' primary>
+            <Button.Content hidden>Ajouter au panier</Button.Content>
+            <Button.Content visible>
+              <Icon name='shop' />
+            </Button.Content>
           </Button>
-            <Button>Delete</Button>
-          </Card.Content>
         </Card>
 
         <Card>
           <Placeholder>
             <Placeholder.Image square />
           </Placeholder>
-
           <Card.Content>
-            <Placeholder>
-              <Placeholder.Header>
-                <Placeholder.Line length='very short' />
-                <Placeholder.Line length='medium' />
-              </Placeholder.Header>
-              <Placeholder.Paragraph>
-                <Placeholder.Line length='short' />
-              </Placeholder.Paragraph>
-            </Placeholder>
+            <Card.Header>Truc à vendre</Card.Header>
+            <Card.Meta>
+              <span className='price'>60€</span>
+            </Card.Meta>
+            <Card.Description>Un truc dont vous avez vraiment besoin</Card.Description>
           </Card.Content>
-
-          <Card.Content extra>
-            <Button primary>
-              Add
+          <Button animated='vertical' primary>
+            <Button.Content hidden>Ajouter au panier</Button.Content>
+            <Button.Content visible>
+              <Icon name='shop' />
+            </Button.Content>
           </Button>
-            <Button>Delete</Button>
-          </Card.Content>
         </Card>
+
         <Card>
           <Placeholder>
             <Placeholder.Image square />
           </Placeholder>
-
           <Card.Content>
-            <Placeholder>
-              <Placeholder.Header>
-                <Placeholder.Line length='very short' />
-                <Placeholder.Line length='medium' />
-              </Placeholder.Header>
-              <Placeholder.Paragraph>
-                <Placeholder.Line length='short' />
-              </Placeholder.Paragraph>
-            </Placeholder>
+            <Card.Header>Truc à vendre</Card.Header>
+            <Card.Meta>
+              <span className='price'>60€</span>
+            </Card.Meta>
+            <Card.Description>Un truc dont vous avez vraiment besoin</Card.Description>
           </Card.Content>
-
-          <Card.Content extra>
-            <Button primary>
-              Add
+          <Button animated='vertical' primary>
+            <Button.Content hidden>Ajouter au panier</Button.Content>
+            <Button.Content visible>
+              <Icon name='shop' />
+            </Button.Content>
           </Button>
-            <Button>Delete</Button>
-          </Card.Content>
         </Card>
+
       </Card.Group>
     </Container>
   );
