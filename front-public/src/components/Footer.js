05bd4cf709c97b74655ef6107401f75f1d64fe7f
Eva Spessotto <eva@spessotto.com>
2019-01-23 12:12:26 +0100

    Changement aligement icones footer

--- front-public/src/components/Footer.js
+++ front-public/src/components/Footer.js
@@ -4,50 +4,58 @@ import { Segment, Container, Grid, Header, Icon, Item, Divider } from 'semantic-
 class Footer extends Component {
   render() {
     return (
-      <Container style={{marginBottom: '20px'}}>
+      <Container style={{ marginBottom: '20px' }}>
         <Divider />
-        <Grid style={{marginTop: '20px'}} stackable>
-          <Grid.Row columns='4'>
-            <Grid.Column>
+        <Grid style={{ marginTop: '20px' }} textAlign='center' stackable>
+          <Grid.Row columns='4' >
+            <Grid.Column textAlign='left'>
               <Header>About</Header>
               <p>About us</p>
               <p>Carrer</p>
               <p>Corporate Sales</p>
               <p>Where to buy</p>
             </Grid.Column>
-            <Grid.Column>
+            <Grid.Column textAlign='left'>
               <Header>Support</Header>
               <p>Contact</p>
               <p>Returns</p>
               <p>Warranty</p>
               <p>Help</p>
             </Grid.Column>
-            <Grid.Column>
+            <Grid.Column textAlign='left'>
               <Header>Legal</Header>
               <p>Privacy</p>
               <p>Terms</p>
               <p>Patends</p>
             </Grid.Column>
-            <Grid.Column>
+            <Grid.Column textAlign='left'>
               <Header>Other</Header>
               <p>Account</p>
               <p>Affiliate Program</p>
             </Grid.Column>
           </Grid.Row>
 
-          <Grid.Row textAlign='center' style={{marginTop: '50px'}}>
+          <Grid.Row  style={{marginBottom: '20px', marginTop: '50px' }}>
             <Grid.Column>
-              <Icon name='facebook' size='large' />
-              <Icon name='instagram' size='large' />
-              <Icon name='youtube' size='large' />
-              <Icon name='twitter' size='large' />
-              <Icon name='pinterest' size='large' />
+              <Icon link name='facebook' size='large' />
+            </Grid.Column>
+            <Grid.Column>
+              <Icon link name='instagram' size='large' />
+            </Grid.Column>
+            <Grid.Column>
+              <Icon link name='youtube' size='large' />
+            </Grid.Column>
+            <Grid.Column>
+              <Icon link name='twitter' size='large' />
+            </Grid.Column>
+            <Grid.Column>
+              <Icon link name='pinterest' size='large' />
             </Grid.Column>
           </Grid.Row>
 
           <Grid.Row textAlign='center'>
             <Grid.Column>
-            <p>@2019 - All right reserved</p>
+              <p>@2019 - All right reserved</p>
             </Grid.Column>
           </Grid.Row>
         </Grid>
