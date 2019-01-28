49be0f47b02fc58f1b0c4fa33de914ab052d5b80
Spessotto <eva@spessotto.com>
2019-01-28 22:25:35 +0100

    Ajout barre de recheche dans la navbar + changement style navbar

--- front-public/src/components/Navigation.js
+++ front-public/src/components/Navigation.js
@@ -1,53 +1,49 @@
-import React, { Component } from 'react';
-import { Menu, Button, Container, Icon, IconGroup } from 'semantic-ui-react'
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import Basket from './basket/Basket';
+import React, { Component } from "react";
+import { Menu, Button, Container, Icon, Search } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import Basket from "./basket/Basket";
 
 class Navigation extends Component {
-  state = {}
+  state = {};
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
   render() {
-    const { activeItem } = this.state
+    const { activeItem } = this.state;
     return (
-      
-        <Menu  fixed='top'>
+      <Menu fixed="top" borderless>
         <Container>
-            <Menu.Item as={ Link } name='super chaussettes' to='/' />
+          <Menu.Item as={Link} name="super chaussettes" to="/"  />
           <Menu.Item
-            name='products'
-            active={activeItem === 'products'}
-            onClick={this.handleItemClick}
+						name="products"
+						as={Link}
+						to='/products'
           />
           <Menu.Item position='right'>
-            <Link to='/mon-panier'>
-              <Button>
-                  <Icon name='shop'/> 
-                  {
-                    this.props.cart.length === 0
-                    ? ''
-                    : this.props.cart.length
-                  }
-              </Button>
-            </Link>
+            <Search />
           </Menu.Item>
+          <Menu.Item>
+            <Button as={Link} to="/mon-panier">
+              <Icon name="shop" />
+              {this.props.cart.length === 0 ? "" : this.props.cart.length}
+            </Button>
+          </Menu.Item>
+
           <Menu.Item>
             <Button primary>Sign in</Button>
           </Menu.Item>
-          <Menu.Item >
+          <Menu.Item>
             <Button secondary>Sign Up</Button>
           </Menu.Item>
-      </Container>
-
-        </Menu>
+        </Container>
+      </Menu>
     );
   }
 }
 
 const mapStateToProps = state => ({
   cart: state.cart.cart
-})
+});
 
 export default connect(mapStateToProps)(Navigation);
