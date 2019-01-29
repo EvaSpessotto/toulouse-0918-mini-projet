5175eb8387caa8a8fcd5917edcc3d2c54a8fddd5
Spessotto <eva@spessotto.com>
2019-01-29 17:13:22 +0100

    Récupération des products

--- front-admin/src/containers/ProductsListContainer.js
+++ front-admin/src/containers/ProductsListContainer.js
@@ -3,10 +3,17 @@ import { Container } from 'semantic-ui-react';
 import ProductsList from '../components/ProductsList';
 import '../style/productsListContainer.scss';
 import axios from 'axios';
+import { connect } from 'react-redux';
+import { fetchProducts, fetchProductsError, fetchProductsSucess} from '../actions';
 
 class ProductsListContainer extends Component {
 
 	componentDidMount() {
+		this.props.fetchProducts()
+		axios.get('http://localhost:5000/api/products')
+			.then(res => res.data)
+			.then(products => this.props.fetchProductsSucess(products))
+			.catch(error => this.props.fetchProductsError(error))
 	}
 	
 	render() {
@@ -18,4 +25,14 @@ class ProductsListContainer extends Component {
 	}
 }
 
-export default ProductsListContainer;
+const mapStateToProps = state => ({
+	products: state.products
+});
+
+const mapDispatchToProps = {
+	fetchProducts, 
+	fetchProductsError,
+	fetchProductsSucess
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductsListContainer);
