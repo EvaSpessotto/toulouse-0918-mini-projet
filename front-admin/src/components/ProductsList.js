3cceae7f243bd0bed178fc9748978bf33846e0cc
Spessotto <eva@spessotto.com>
2019-01-29 17:32:07 +0100

    Afficahge de tous les éléments

--- front-admin/src/components/ProductsList.js
+++ front-admin/src/components/ProductsList.js
@@ -1,50 +1,42 @@
-import React, { Component } from 'react';
-import { Table, Button, Image } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Table, Button, Image } from "semantic-ui-react";
+import moment from 'moment';
 
-
-class ProductsList extends Component {
-	render() {
-		return (
-			<Table celled striped>
-				<Table.Header>
-					<Table.Row>
-						<Table.HeaderCell colSpan='8'>Products</Table.HeaderCell>
-					</Table.Row>
-				</Table.Header>
-
-				<Table.Body>
-					<Table.Row>
-						<Table.Cell>
-							<Image src='' size='mini' />
-						</Table.Cell>
-						<Table.Cell>
-							name
-						</Table.Cell>
-						<Table.Cell>
-							description
-						</Table.Cell>
-						<Table.Cell>
-							Stock
-						</Table.Cell>
-						<Table.Cell>
-							Price
-						</Table.Cell>
-						<Table.Cell>
-							Date
-						</Table.Cell>
-						<Table.Cell>
-							reference
-						</Table.Cell>
-						<Table.Cell textAlign='right'>
-							<Button icon='edit' color='blue' />
-							<Button icon='close' color='red' />
-						</Table.Cell>
-					</Table.Row>
-				</Table.Body>
-				
-			</Table>
-		);
-	}
-}
+const ProductsList = ({ products }) => {
+  return (
+      <Table.Body>
+        {products.map(product => {
+          const {
+            name,
+            picture,
+            description,
+            stock,
+            price,
+            createdAt,
+            reference
+          } = product;
+          return (
+            <Table.Row>
+              <Table.Cell>
+                <Image src={picture} size="mini" />
+              </Table.Cell>
+              <Table.Cell>{name}</Table.Cell>
+              <Table.Cell>{description}</Table.Cell>
+              <Table.Cell>{stock} items</Table.Cell>
+              <Table.Cell>{price}€</Table.Cell>
+              <Table.Cell>{moment(createdAt).format("l")}</Table.Cell>
+              <Table.Cell>n°{reference}</Table.Cell>
+              <Table.Cell textAlign="right">
+                <Button icon="edit" color="blue" />
+              </Table.Cell>
+              <Table.Cell>
+                <Button icon="close" color="red" />
+              </Table.Cell>
+            </Table.Row>
+          );
+        })}
+      </Table.Body>
+  );
+};
 
 export default ProductsList;
--- front-admin/src/containers/ProductsListContainer.js
+++ front-admin/src/containers/ProductsListContainer.js
@@ -1,38 +1,62 @@
-import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react';
-import ProductsList from '../components/ProductsList';
-import '../style/productsListContainer.scss';
-import axios from 'axios';
-import { connect } from 'react-redux';
-import { fetchProducts, fetchProductsError, fetchProductsSucess} from '../actions';
+import React, { Component } from "react";
+import { Container, Table } from "semantic-ui-react";
+import ProductsList from "../components/ProductsList";
+import "../style/productsListContainer.scss";
+import axios from "axios";
+import { connect } from "react-redux";
+import {
+  fetchProducts,
+  fetchProductsError,
+  fetchProductsSucess
+} from "../actions";
 
 class ProductsListContainer extends Component {
+  componentDidMount() {
+    this.props.fetchProducts();
+    axios
+      .get("http://localhost:5000/api/products")
+      .then(res => res.data)
+      .then(products => this.props.fetchProductsSucess(products))
+      .catch(error => this.props.fetchProductsError(error));
+  }
 
-	componentDidMount() {
-		this.props.fetchProducts()
-		axios.get('http://localhost:5000/api/products')
-			.then(res => res.data)
-			.then(products => this.props.fetchProductsSucess(products))
-			.catch(error => this.props.fetchProductsError(error))
-	}
-	
-	render() {
-		return (
-			<Container id="products-list-container">
-				<ProductsList />
-			</Container>
-		);
-	}
+  render() {
+    return (
+      <Container id="products-list-container">
+        <Table celled striped>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Picture</Table.HeaderCell>
+              <Table.HeaderCell>Description</Table.HeaderCell>
+              <Table.HeaderCell>Stock</Table.HeaderCell>
+              <Table.HeaderCell>Price</Table.HeaderCell>
+              <Table.HeaderCell>Created At</Table.HeaderCell>
+              <Table.HeaderCell>Reference</Table.HeaderCell>
+              <Table.HeaderCell>Edit</Table.HeaderCell>
+              <Table.HeaderCell>Delete</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <ProductsList products={this.props.products} />
+
+        </Table>
+      </Container>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-	products: state.products
+  products: state.products
 });
 
 const mapDispatchToProps = {
-	fetchProducts, 
-	fetchProductsError,
-	fetchProductsSucess
-}
+  fetchProducts,
+  fetchProductsError,
+  fetchProductsSucess
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProductsListContainer);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProductsListContainer);
